516d063b76b3ee3620c7d073fae2386281c8bbf4
Siarhei <sergej.vasilewsckij@yandex.ru>
2023-02-25 02:58:29 +0300

    item dissapear task, item movement task

---
+++ Assets/CodeBase/TaskRunner/DissapearTask.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) 2012-2021 FuryLion Group. All Rights Reserved.
+
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using CodeBase.Items;
+
+namespace CodeBase.TaskRunner
+{
+    public class DissapearTask : ITask
+    {
+        private const float ScaleDuration = 0.3f;
+        private const float FadeDuration = 0.3f;
+
+        private readonly IEnumerable<IGriddable> _items;
+
+        public DissapearTask(IEnumerable<IGriddable> items)
+        {
+            _items = items;
+        }
+
+        public async UniTask Execute(CancellationToken cancellationToken = default)
+        {
+            Sequence sequence = DOTween.Sequence();
+
+            foreach (IGriddable griddable in _items)
+            {
+                _ = sequence
+                    .Join(griddable.Item.transform.DOScale(Vector3.zero, ScaleDuration))
+                    .Join(griddable.Item.SpriteRenderer.DOFade(0, FadeDuration));
+            }
+
+            await sequence.WithCancellation(cancellationToken);
+
+            //reclaim items
+        }
+
+    }
+}
---
+++ Assets/CodeBase/TaskRunner/MoveTask.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) 2012-2021 FuryLion Group. All Rights Reserved.
+
+using DG.Tweening;
+using CodeBase.Items;
+
+namespace CodeBase.TaskRunner
+{
+    public class MoveTask
+    {
+        private const float MoveDuration = 0.3f;
+        
+        private GameBoard.GameBoard _gameBoard;
+
+        public MoveTask(GameBoard.GameBoard gameBoard)
+        {
+            _gameBoard = gameBoard;
+        }
+
+        public void CreatePath()
+        {
+            Sequence sequence = DOTween.Sequence();
+            for (int i = 0; i < _gameBoard.Columns; i++)
+            {
+                for (int j = 0; j < _gameBoard.Rows; j++)
+                {
+                    Cell cell = _gameBoard[i, j];
+                    if (!cell.IsEmpty)
+                    {
+                        Item item = cell.Item;
+                        cell.Clear();
+                        BoardPosition destination = Create(i, j);
+                        _gameBoard[destination.PosX, destination.PosY].Item = item;
+                        sequence.Insert(j/10,
+                            item.transform.DOMove(destination.ToVector(), MoveDuration));
+                    }
+                }
+            }
+            sequence.SetEase(Ease.Linear);
+        }
+
+        private BoardPosition Create(int column, int row)
+        {
+            bool isonBoard = _gameBoard.IsOnBoard(column, row - 1);
+            bool isEmpty = false;
+            if (isonBoard)
+                isEmpty = _gameBoard[column, row - 1].IsEmpty;
+            if (isonBoard && isEmpty)
+            {
+                return Create(column, row - 1);
+            }
+            return new BoardPosition(column, row);
+        }
+    }
+}
